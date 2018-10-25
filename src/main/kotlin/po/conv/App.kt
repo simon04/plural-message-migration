76cce5600ef4b70390445d681ec859304fcb4968
Simon Legner <Simon.Legner@gmail.com>
2018-10-25 18:32:58 +0200

    BE

--- src/main/kotlin/po/conv/App.kt
+++ src/main/kotlin/po/conv/App.kt
@@ -5,9 +5,24 @@ import org.fedorahosted.tennera.jgettext.PoWriter
 import java.io.File
 
 fun main(args: Array<String>) {
+    convertBE()
     convertCS()
     convertSK()
     convertUK()
+}
+
+private fun convertBE() {
+    val file = File("po/be.po")
+    val catalog = PoParser().parseCatalog(file)
+    val header = catalog.locateHeader()
+    header.msgstr = header.msgstr.replace(
+            "Plural-Forms: nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && \"\n\"n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);",
+            "Plural-Forms: nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);"
+    )
+    catalog.filter { it.isPlural && it.msgstrPlural.size == 3 }.forEach {
+        it.msgstrPlural.add(it.msgstrPlural[2])
+    }
+    PoWriter().write(catalog, file)
 }
 
 private fun convertCS() {
