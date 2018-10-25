29a05ced9fbf243e0913d04c25df0a42453e8697
Simon Legner <Simon.Legner@gmail.com>
2018-10-25 19:16:31 +0200

    PL, RU

--- src/main/kotlin/po/conv/App.kt
+++ src/main/kotlin/po/conv/App.kt
@@ -9,6 +9,8 @@ fun main(args: Array<String>) {
     convertCS()
     convertLT()
     convertSK()
+    convertPL()
+    convertRU()
     convertUK()
 }
 
@@ -69,6 +71,34 @@ private fun convertSK() {
         it.msgstrPlural[1] = p2
         it.msgstrPlural[2] = p0
         it.msgstrPlural.add(p0)
+    }
+    PoWriter().write(catalog, file)
+}
+
+private fun convertPL() {
+    val file = File("po/pl.po")
+    val catalog = PoParser().parseCatalog(file)
+    val header = catalog.locateHeader()
+    header.msgstr = header.msgstr.replace(
+            "Plural-Forms: nplurals=3; plural=n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 \"\n\"|| n%100>=20) ? 1 : 2;",
+            "Plural-Forms: nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);"
+    )
+    catalog.filter { it.isPlural && it.msgstrPlural.size == 3 }.forEach {
+        it.msgstrPlural.add(it.msgstrPlural[2])
+    }
+    PoWriter().write(catalog, file)
+}
+
+private fun convertRU() {
+    val file = File("po/ru.po")
+    val catalog = PoParser().parseCatalog(file)
+    val header = catalog.locateHeader()
+    header.msgstr = header.msgstr.replace(
+            "Plural-Forms: nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && \"\n\"n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;",
+            "Plural-Forms: nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);"
+    )
+    catalog.filter { it.isPlural && it.msgstrPlural.size == 3 }.forEach {
+        it.msgstrPlural.add(it.msgstrPlural[2])
     }
     PoWriter().write(catalog, file)
 }
